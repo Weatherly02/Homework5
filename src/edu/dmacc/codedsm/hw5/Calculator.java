081eb0a92f22af236195e1c2640a403d5e126a8e
Weatherly02 <47132416+Weatherly02@users.noreply.github.com>
2019-02-20 19:25:43 -0600

    adding math.floorDiv to HW5

--- src/edu/dmacc/codedsm/hw5/Calculator.java
+++ src/edu/dmacc/codedsm/hw5/Calculator.java
@@ -4,27 +4,27 @@ public class Calculator {
     public static void main(String[] args) {
         int number = 27;
         double squareRoot = Math.sqrt(number);
+        System.out.println("Number : " + number);
+        System.out.println("Square Root : " + squareRoot);
 
         double a = 28.0;
         double b = 5.0;
         double round = Math.round(a / b);
         double mathFloor = Math.floor(a / b);
-
-        int random = (int) (Math.random() * 10 + 1);
-
-        double pow = Math.pow(2.2, 4);
-
-        System.out.println("number : " + number);
-        System.out.println("Square Root : " + squareRoot);
-
-        System.out.println("division : " + (a / b));
+        System.out.println("Division : " + (a / b));
         System.out.println("Division Rounded up : " + round);
         System.out.println("Division rounds down : " + mathFloor);
 
-        System.out.println("2.2 to the 4th power : " + pow);
+        int c = 28;
+        int d = 5;
+        double mathFloorTwo = Math.floorDiv(c, d);
+        System.out.println("Division again : " + mathFloorTwo);
 
+        int random = (int) (Math.random() * 10 + 1);
         System.out.println("Random int : " + random);
 
+        double pow = Math.pow(2.2, 4);
+        System.out.println("2.2 to the 4th power : " + pow);
 
     }
 }
